d919f88cc281c368ce3a40125301d3436f321cf1
dnl886 <61706364+dnl886@users.noreply.github.com>
2020-06-23 15:14:36 -0500

    Initial commit

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class EditorSnap : MonoBehaviour {
+    
+    [SerializeField] [Range(1f,20f)] float gridSize = 10f; //range restricts the range of the field
+
+    TextMesh texMesh;
+
+    private void Start()
+    {
+        
+    }
+    // Use this for initialization
+    private void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / 10) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / 10) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        texMesh.text = snapPos.x/ gridSize + ", " + snapPos.z/gridSize; 
+
+    }
+}
