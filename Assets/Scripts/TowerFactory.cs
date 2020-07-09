bb645e4c982a39f350ebe4f3e767087af7e8ce8c
dnl886 <61706364+dnl886@users.noreply.github.com>
2020-07-09 02:51:15 -0500

    Ring Buffers (partly)

---
+++ Assets/Scripts/TowerFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFactory : MonoBehaviour {
+
+    [SerializeField] int towerLimit = 8;
+    [SerializeField] Tower towerPrefab;
+    bool isPlaceable;
+    
+    public void AddTower(Waypoint baseWaypoint)
+    {
+        var towers = FindObjectsOfType<Tower>();
+        int numTowers = towers.Length;
+
+        if(numTowers < towerLimit)
+        {
+            InstantiateNewTower(baseWaypoint);
+        }
+        else
+        {
+            MoveExistingTower();
+        }
+    }
+
+    private void MoveExistingTower()
+    {
+        print("Tower Limit Reached!");
+    }
+
+    private void InstantiateNewTower(Waypoint baseWaypoint)
+    {
+        Instantiate(towerPrefab, baseWaypoint.transform.position, Quaternion.identity);
+        baseWaypoint.isPlaceable = false; 
+    }
+}
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -10,7 +10,6 @@ public class Waypoint : MonoBehaviour {
 
     public bool isExplored = false;
     public bool isPlaceable = true;
-    [SerializeField] Tower towerPrefab;
 
     public Waypoint exploredFrom;
     Vector2Int gridPos;
@@ -47,9 +46,7 @@ public class Waypoint : MonoBehaviour {
         {
             if (isPlaceable)
             {
-                print(gameObject.name + " tower placement");
-                Instantiate(towerPrefab, transform.position, Quaternion.identity);
-                isPlaceable = false;
+                FindObjectOfType<TowerFactory>().AddTower(this);
             }
             else
             {
