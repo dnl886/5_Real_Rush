fabb4c026fe2607db1d3596413466f5992e90a70
Dan Belepe <danbelepe@gmail.com>
2020-07-21 17:29:31 -0500

    Score text works

--- Assets/ScoreText.cs
+++ Assets/ScoreText.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreText : MonoBehaviour {
-
+    [SerializeField] List<EnemyMovement> enemyPrefab = new List<EnemyMovement>();
+    [SerializeField] Text scoreText;
+    [SerializeField] int score = 0;
+    [SerializeField] int enemyScoreMultiplier = 1;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		scoreText.text = score.ToString();
+        foreach(EnemyMovement enemies in enemyPrefab)
+        {
+            score += enemyScoreMultiplier;
+        }
 	}
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour {
     [Range(0.1f, 120f)]
@@ -10,20 +11,32 @@ public class EnemySpawner : MonoBehaviour {
 
     [SerializeField] Transform enemyParentTransform;
 
-	// Use this for initialization
-	void Start ()
+    [SerializeField] Text spawnedEnemies;
+    [SerializeField] int score = 0;
+    [SerializeField] int enemyScoreMultiplier = 1;
+
+    // Use this for initialization
+    void Start ()
     {
         StartCoroutine(RepeatedlySpawnEnemies());
+        spawnedEnemies.text = score.ToString();
 	}
 	
     IEnumerator RepeatedlySpawnEnemies()
     {
         while (true) // forever
         {
+            AddScore();
             var newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
 
             newEnemy.transform.parent = enemyParentTransform;
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
+    }
+
+    void AddScore()
+    {
+        score++;
+        spawnedEnemies.text = score.ToString();
     }
 }
